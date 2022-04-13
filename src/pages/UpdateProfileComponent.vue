9e11c7e2e2bb486f95fd56533ebccebded0f09f5
HOANG TRUNG TIEN <hoangtrungtien99@gmail.com>
2022-04-13 08:01:04 +0700

    Done

--- src/pages/UpdateProfileComponent.vue
+++ src/pages/UpdateProfileComponent.vue
@@ -3,6 +3,7 @@
     <div class="bg-grey-lighter min-h-screen flex flex-col my-20">
         <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
             <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                <img :src='previewImage' alt="">
                 <form class="w-full p-10 rounded mx-auto flex flex-col form-wrapper shadow-lg shadow-black-500/50" @submit.prevent="updateProfile" method="" action="">
                     <h1 class="mb-8 text-3xl text-center text-bold font-black">Update profile</h1>
                     <input type="text" class="block border border-grey-light w-full p-3 rounded mb-4" name="fullname" maxlength="10" placeholder="Full Name" v-model="fullName" />
@@ -87,6 +88,15 @@ export default {
                 alert(e)
             }
         },
+        async getUser() {
+            try{
+                const user = await userServices.GetUser();
+                console.log(user.user.full_avatar_path)
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        ,
         logOut() {
             localStorage.removeItem('token');
             setTimeout(() => {
@@ -95,7 +105,7 @@ export default {
         }
     },
     mounted() {
-
+        
     },
 }
 </script>
